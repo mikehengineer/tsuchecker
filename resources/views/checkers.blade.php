8092b2d3b51350a4cdcf38a4881ce2fba30e1f90
mikehengineer <mikehengineer@gmail.com>
2018-07-06 00:16:31 -0400

    fixed frontend

--- resources/views/checkers.blade.php
+++ resources/views/checkers.blade.php
@@ -9,10 +9,9 @@
 </head>
 <body>
 <div class="container">
+
     <nav>
-
         mikehengineer@gmail.com
-
     </nav>
 
     <div id="splashouter">
@@ -26,68 +25,15 @@
     <div class="content">
 
         <div class="card">
-            <img src = "https://images.unsplash.com/photo-1517646287270-a5a9ca602e5c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8130ea340b0db93fe272b68d4e68442f&auto=format&fit=crop&w=1650&q=80">
-
-        </div>
-
-        <div class="card">
-
-            <img src="https://images.unsplash.com/photo-1507089947368-19c1da9775ae?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4d2529242e0b4fadad755467e572682a&auto=format&fit=crop&w=1655&q=80">
-
-        </div>
-
-        <div class="card">
-
-            <div class="card-body">
-                <h4>Add Datapoints</h4>
-                <form METHOD="POST" action="/addpoints">
-
-                    {{ csrf_field() }}
-
-                    <div class="form-group">
-                        <label>Device #</label>
-                        <input type="text" name="device" placeholder="Name" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label># of Datapoints</label>
-                        <input type="text" name="datapoints" placeholder="Type" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Add</button>
-                    </div>
-                    @if ($flash = session('deviceMsg'))
-                        <div class="alert alert-success" role="alert" id="deviceMsg">
-                            {{ $flash }}
-                        </div>
-                    @endif
-                    @if($errors->has('device') || $errors->has('datapoints') || count($errors->notfound->all()))
-                        <div class="form-group">
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                    @foreach ($errors->notfound->all() as $notfound)
-                                        <li>{{ $notfound }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        </div>
-                    @endif
-                </form>
-            </div>
-        </div>
-
-        <div class="card">
             <p>
-
-                Fusce accumsan sodales neque ac ultricies. Sed at lorem blandit, commodo neque eget, mattis arcu. In posuere egestas malesuada. Praesent in finibus ipsum. Integer consectetur, arcu vitae rhoncus sollicitudin, ante neque consectetur sem, nec rhoncus urna
-                tellus non quam. Quisque et tincidunt metus. Aliquam commodo eros id felis placerat, sit amet gravida nunc venenatis. In pharetra lacus dolor, ac congue ligula ultrices quis. Vestibulum feugiat eros vel quam semper, non eleifend arcu mattis. Nunc
-                semper, velit sed rhoncus scelerisque, ex sapien varius nunc, et porta ipsum felis nec nibh. Nulla rhoncus est vestibulum bibendum euismod. Aenean ornare dolor felis, quis pulvinar tellus consequat efficitur.
+            <h1>Welcome to TSU Checkers</h1>
+            </p>
+            <p>
+                Simply visit this page with an 8x8 JSON grid consisting of red ("R") or black ("B") squares.  This website will return a JSON array consisting of the starting point, direction and color of any four in a row combinations.
             </p>
         </div>
 
     </div>
-
+</div>
 </body>
 </html>
